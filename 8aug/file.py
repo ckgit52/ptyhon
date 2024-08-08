423bd972cb4cbda87378b72f2456474514417212
chandan <chandankumar.btele20@pec.edu.in>
2024-08-08 15:42:42 +0530

    first commit

---
+++ 8aug/file.py
@@ -0,0 +1,30 @@
+# comment are those lines of codes in python whihc does not run , it is only used to indetify the code which is written in program
+
+print("saksham")
+
+# following is name of viabhav
+
+print("viabhav nassa")
+
+# ###
+# jhfdhjd
+# jhsdhjsjhhfddhj
+# ###
+
+
+# jhfhdjhfd
+# jhfhdjhfd
+# jhfdhfbdjhfd
+
+"""
+MULTY LINE COMMENTS IN PYTHON
+hello i m 
+
+commented in multiple lines
+
+print("i m inner")
+
+in   ("""    """)
+
+
+"""
